815deb51fba55fd5b51cd30816cef78c4aaf9b01
benoniokaikoi <derhain@gmail.com>
2025-10-22 15:05:12 +0000

    Fix SQLAlchemy import

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from config import Config
-from models import db
+from models import db   # this is fine here
 from routes.auth_routes import auth_bp
 
+##check
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
@@ -16,10 +17,15 @@ jwt = JWTManager(app)
 def home():
     return jsonify({"message": "Welcome to Spaklean Backend API!"})
 
-# Register Blueprints
 app.register_blueprint(auth_bp)
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5001)
+
+
+
+
+
