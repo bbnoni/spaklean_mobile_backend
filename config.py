ed3395d9a0674670c5359b39fd1094713bc499a4
benoniokaikoi <derhain@gmail.com>
2025-10-22 15:57:11 +0000

    Switch to psycopg v3 for Python 3.13 compatibility

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://spaklean_db_mobile_user:BkBXGsabaLp23dnKxohjEH2N9WXFUuCZ@dpg-d3selkk9c44c73com7eg-a.oregon-postgres.render.com/spaklean_db_mobile"
+        "postgresql+psycopg://spaklean_db_mobile_user:BkBXGsabaLp23dnKxohjEH2N9WXFUuCZ@dpg-d3selkk9c44c73com7eg-a.oregon-postgres.render.com/spaklean_db_mobile"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "spaklean_secret_key"
